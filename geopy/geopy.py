5a8fdfda51ba9a6b86c1d75a2e6ff90f7b51c413
nisketch <nimrodogoro@gmail.com>
2024-05-07 09:51:58 +0300

    COMMIT

--- geopy/geopy.py
+++ geopy/geopy.py
@@ -3,6 +3,6 @@ import ipyleaflet
 
 class Map(ipyleaflet.Map()):
     
-    def __init__(self, **kwargs):
-        super()__init__(**kwargs)
+    def __init__(self,center=(-10, 35 ), zoom=5,  **kwargs):
+        super()__init__(center=center, zoom=zoom, **kwargs) # type: ignore
         self.add_control(ipyleaflet.LayersControl())
