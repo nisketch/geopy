4b92f50317d4941438bfde184ba650d69f7f4799
nisketch <nimrodogoro@gmail.com>
2024-05-07 16:49:15 +0300

    commit

--- geopy/__init__.py
+++ geopy/__init__.py
@@ -3,3 +3,5 @@
 __author__ = """Nimrod Omanga"""
 __email__ = "nimrodogoro@gmail.com"
 __version__ = "0.0.1"
+
+from .geopy import Map
--- geopy/geopy.py
+++ geopy/geopy.py
@@ -1,7 +1,7 @@
 """Main module."""
 import ipyleaflet
 
-class Map(ipyleaflet.Map()):
+class Map(ipyleaflet.Map):
     
     def __init__(self, center=[-10, 35 ], zoom=5,  **kwargs):
      super().__init__(center=center, zoom=zoom, **kwargs) 
